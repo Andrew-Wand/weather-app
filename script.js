d6f4898bba8085a9e3cab3cbafd1513beff38eef
Andrew Wand <andrewwands@gmail.com>
2022-03-31 13:33:18 -0500

    API search working. css DOM init commit

---
+++ script.js
@@ -0,0 +1,69 @@
+const weatherContent = document.getElementById('weather-content');
+
+const weatherContainer = document.getElementById('weather-container');
+
+const searchBtn = document.getElementById('search-btn');
+const searchInput = document.getElementById('search-bar');
+
+
+
+
+
+
+
+
+
+async function getWeather(e) {
+    e.preventDefault();
+
+
+
+    let term = searchInput.value;
+
+    // Reset DOM input
+    searchInput.value = '';
+
+
+    // Fetch data from open weather api
+    const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${term}&appid=29d36979bd00b16fe04087ee6e2fad28&units=imperial `, {
+        mode: 'cors'
+    });
+    const weatherData = await response.json();
+
+
+
+
+
+    // Weather info DOM
+    const weatherIcon = `http://openweathermap.org/img/w/${weatherData.weather[0].icon}.png`
+
+
+    weatherContent.innerHTML = `
+    
+    <h1> ${weatherData.main.temp}°F</h1>
+    
+    <h1 class='city-name'> ${weatherData.name}, ${weatherData.sys.country}</h1>
+
+    
+    <h1>
+    <img src=${weatherIcon}>
+    ${weatherData.weather[0].description}
+    </h1>
+    
+    `
+
+    weatherContainer.classList.add('active');
+
+
+
+
+
+}
+
+
+
+
+
+
+
+searchBtn.addEventListener('click', getWeather);
